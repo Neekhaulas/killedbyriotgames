474bd4d46e2f114431d42755bbd57e7a84d2df58
LaitEntier <axelzinc@hotmail.fr>
2022-06-23 19:47:32 +0200

    OPL + type 'competition'

--- types/Product.ts
+++ types/Product.ts
@@ -4,7 +4,8 @@ export enum ProductType {
     CHAMPION = 'champion',
     ITEM = 'item',
     APP = 'app',
-    API = 'api'
+    API = 'api',
+    COMPETITION = 'competition'
 }
 
 export interface Product {
